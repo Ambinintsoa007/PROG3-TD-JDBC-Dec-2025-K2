75d9d172c57364dce337fb03695b3ff02be1a162
Ambinintsoa007 <hei.ambinintsoa@gmail.com>
2026-01-21 17:09:31 +0300

    chore: updating DBConnection class with Mrs code

--- PROG3-TD-JDBC-Dec-2025/src/main/java/Classes/DBConnection.java
+++ PROG3-TD-JDBC-Dec-2025/src/main/java/Classes/DBConnection.java
@@ -1,15 +1,28 @@
 package Classes;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    public Connection getDBConnection() throws SQLException {
-        String url = System.getenv("JDBC_URL");
-        String user = System.getenv("USERNAME");
-        String password = System.getenv("PASSWORD");
 
-        return DriverManager.getConnection(url, user, password);
+    public Connection getConnection() {
+        try {
+            String jdbcURl = System.getenv("JDBC_URl"); //
+            String user = System.getenv("USER"); //mini_dish_db_manager
+            String password = System.getenv("PASSWORD"); //123456
+            return DriverManager.getConnection("jdbc:postgresql://localhost:5432/mini_dish_db", "postgres", "postgres");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void closeConnection(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
