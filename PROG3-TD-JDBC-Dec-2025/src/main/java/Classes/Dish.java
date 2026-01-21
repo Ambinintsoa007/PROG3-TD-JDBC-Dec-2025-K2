4c95bb753cd9dfa372a05b9d4d2dc8a21ea1c447
Ambinintsoa007 <hei.ambinintsoa@gmail.com>
2026-01-21 17:08:31 +0300

    chore: updating Dish class with Mrs code

--- PROG3-TD-JDBC-Dec-2025/src/main/java/Classes/Dish.java
+++ PROG3-TD-JDBC-Dec-2025/src/main/java/Classes/Dish.java
@@ -1,59 +1,112 @@
 package Classes;
-import lombok.*;
 
 import java.util.List;
-
-@Getter
-@EqualsAndHashCode
-@Setter
+import java.util.Objects;
 
 public class Dish {
-    private int id;
-    private String nom;
-    private DishTypeEnum type;
-    private List<Ingredient> ingredient;
+    private Integer id;
+    private Double price;
+    private String name;
+    private DishTypeEnum dishType;
+    private List<Ingredient> ingredients;
 
-    public Dish(int id, String nom, DishTypeEnum type, List<Ingredient> ingredient) {
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Double getDishCost() {
+        double totalPrice = 0;
+        for (int i = 0; i < ingredients.size(); i++) {
+            Double quantity = ingredients.get(i).getQuantity();
+            if(quantity == null) {
+                throw new RuntimeException("...");
+            }
+            totalPrice = totalPrice + ingredients.get(i).getPrice() * quantity;
+        }
+        return totalPrice;
+    }
+
+    public Dish() {
+    }
+
+    public Dish(Integer id, String name, DishTypeEnum dishType, List<Ingredient> ingredients) {
         this.id = id;
-        this.nom = nom;
-        this.type = type;
-        this.ingredient = ingredient;
+        this.name = name;
+        this.dishType = dishType;
+        this.ingredients = ingredients;
+    }
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public DishTypeEnum getDishType() {
+        return dishType;
+    }
+
+    public void setDishType(DishTypeEnum dishType) {
+        this.dishType = dishType;
+    }
+
+    public List<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<Ingredient> ingredients) {
+        if (ingredients == null) {
+            this.ingredients = null;
+            return;
+        }
+        for (int i = 0; i < ingredients.size(); i++) {
+            ingredients.get(i).setDish(this);
+        }
+        this.ingredients = ingredients;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Dish dish = (Dish) o;
+        return Objects.equals(id, dish.id) && Objects.equals(name, dish.name) && dishType == dish.dishType && Objects.equals(ingredients, dish.ingredients);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, dishType, ingredients);
     }
 
     @Override
     public String toString() {
         return "Dish{" +
                 "id=" + id +
-                ", nom='" + nom + '\'' +
-                ", type=" + type +
-                ", ingredient=" + ingredient +
+                ", price=" + price +
+                ", name='" + name + '\'' +
+                ", dishType=" + dishType +
+                ", ingredients=" + ingredients +
                 '}';
     }
 
-    /*public double getDishCost() {
-        double totalCost = 0;
-        if (this.ingredient != null) {
-            for (Ingredient ingredient : this.ingredient) {
-                totalCost += ingredient.getPrice();
-            }
+    public Double getGrossMargin() {
+        if (price == null) {
+            throw new RuntimeException("Price is null");
         }
-        return totalCost;
-    }*/
-    public Double getDishCost() {
-        if (this.ingredient == null || this.ingredient.isEmpty()) {
-            return 0.0;
-        }
-
-        double totalCost = 0.0;
-
-        for (Ingredient ingredient : this.ingredient) {
-            if (ingredient.getRequiredQuantity() == null) {
-                throw new RuntimeException("Impossible de calculer le coût : la quantité nécessaire de l'ingrédient '"
-                        + ingredient.getName() + "' est inconnue");
-            }
-            totalCost += ingredient.getPrice() * ingredient.getRequiredQuantity();
-        }
-
-        return totalCost;
+        return price - getDishCost();
     }
 }
