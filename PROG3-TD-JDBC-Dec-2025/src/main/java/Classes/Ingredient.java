ba723114d97d103dbd5934a81c4806d87edfc159
Ambinintsoa007 <hei.ambinintsoa@gmail.com>
2026-01-21 17:09:06 +0300

    chore: updating Ingredient class with Mrs code

--- PROG3-TD-JDBC-Dec-2025/src/main/java/Classes/Ingredient.java
+++ PROG3-TD-JDBC-Dec-2025/src/main/java/Classes/Ingredient.java
@@ -1,23 +1,91 @@
 package Classes;
 
-import lombok.Getter;
+import java.util.Objects;
 
-@Getter
 public class Ingredient {
-    private int id;
+    private Integer id;
     private String name;
-    private double price;
     private CategoryEnum category;
+    private Double price;
     private Dish dish;
-    private Double requiredQuantity;
+    private Double quantity;
 
-    public Ingredient(int id, String name, double price, CategoryEnum category, Dish dish, Double requiredQuantity) {
+    public Double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Double quantity) {
+        this.quantity = quantity;
+    }
+
+    public Ingredient() {
+    }
+
+    public Ingredient(Integer id) {
+        this.id = id;
+    }
+
+    public Ingredient(Integer id, String name, CategoryEnum category, Double price) {
         this.id = id;
         this.name = name;
-        this.price = price;
         this.category = category;
+        this.price = price;
+    }
+
+    public String getDishName() {
+        return dish == null ? null : dish.getName();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public CategoryEnum getCategory() {
+        return category;
+    }
+
+    public void setCategory(CategoryEnum category) {
+        this.category = category;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Dish getDish() {
+        return dish;
+    }
+
+    public void setDish(Dish dish) {
         this.dish = dish;
-        this.requiredQuantity = requiredQuantity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Ingredient that = (Ingredient) o;
+        return Objects.equals(id, that.id) && Objects.equals(name, that.name) && category == that.category && Objects.equals(price, that.price) && Objects.equals(dish, that.dish);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, category, price, dish);
     }
 
     @Override
@@ -25,21 +93,10 @@ public class Ingredient {
         return "Ingredient{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
-                ", price=" + price +
                 ", category=" + category +
-                ", required_quantity=" + requiredQuantity +
+                ", price=" + price +
+                ", dishName=" + getDishName() +
+                ", quantity=" + quantity +
                 '}';
-    }
-
-    public Double getRequiredQuantity() {
-        return requiredQuantity;
-    }
-
-    public void setRequiredQuantity(Double requiredQuantity) {
-        this.requiredQuantity = requiredQuantity;
-    }
-
-    public String getDishName() {
-        throw new RuntimeException("Not implemented yet");
     }
 }
