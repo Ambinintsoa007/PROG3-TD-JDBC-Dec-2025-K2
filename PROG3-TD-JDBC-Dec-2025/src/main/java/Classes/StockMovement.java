df2ac3359e7611cc15070eca780554d31e8ea9e3
Ambinintsoa007 <hei.ambinintsoa@gmail.com>
2026-01-28 13:30:22 +0300

    feat: add StockMovement class

---
+++ PROG3-TD-JDBC-Dec-2025/src/main/java/Classes/StockMovement.java
@@ -0,0 +1,28 @@
+package Classes;
+
+import java.time.Instant;
+
+public class StockMovement {
+    private Integer id;
+    private StockValue value;
+    private MovementTypeEnum type;
+    private Instant creationDatetime;
+
+    // Constructeurs
+    public StockMovement() {}
+
+    // Getters/Setters
+    public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
+
+    public StockValue getValue() { return value; }
+    public void setValue(StockValue value) { this.value = value; }
+
+    public MovementTypeEnum getType() { return type; }
+    public void setType(MovementTypeEnum type) { this.type = type; }
+
+    public Instant getCreationDatetime() { return creationDatetime; }
+    public void setCreationDatetime(Instant creationDatetime) {
+        this.creationDatetime = creationDatetime;
+    }
+}
